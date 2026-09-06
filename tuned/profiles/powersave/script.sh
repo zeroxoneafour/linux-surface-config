aef5dc0ac4c3c2289dae1faecfa561fdecaf4f16
Vaughan Milliman <vaughan@vaughanm.xyz>
2026-09-06 03:51:32 -0400

    update tuned

--- tuned/profiles/powersave/script.sh
+++ tuned/profiles/powersave/script.sh
@@ -2,20 +2,6 @@
 
 . /usr/lib/tuned/functions
 
-disable_cores() {
-    # cpu0 should always be a p core
-    cpu0_freq=$(< /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
-    for dev in /sys/devices/system/cpu/cpu[0-9]*/; do
-        # I guess this is the only way to distinguish P-cores?
-        maxfreq=$(< $dev/cpufreq/cpuinfo_max_freq)
-        # use this line to disable p cores
-        #if (( maxfreq >= cpu0_freq)); then
-        if (( maxfreq < cpu0_freq )); then
-            echo 0 > $dev/online
-        fi
-    done
-}
-
 kscreen_doctor() {
     local target_mode="$1"
     # get all users with graphical session (seatX)
@@ -28,34 +14,55 @@ kscreen_doctor() {
     done
 }
 
+set_rapl_limit() {
+    local rapl_dev=""
+    for rapl in $(ls -d /sys/class/powercap/intel-rapl:*); do
+        if [ $(cat "$rapl/name") = $1 ]; then
+            rapl_dev=$rapl
+            break
+        fi
+    done
+    if [ -z $rapl_dev ]; then
+        return 0
+    fi
+    echo $2 | tee $rapl_dev/constraint_0_power_limit_uw
+    if [ -n "$3" ]; then
+        echo $3 | tee $rapl_dev/constraint_1_power_limit_uw
+    else
+        echo $2 | tee $rapl_dev/constraint_1_power_limit_uw
+    fi
+}
+
 start() {
-    [ "$USB_AUTOSUSPEND" = 1 ] && enable_usb_autosuspend
+    enable_usb_autosuspend
     enable_wifi_powersave
+    set_hda_intel_powersave 1
+    enable_cpu_multicore_powersave
     # use this line for i915, use enabled line for xe driver
     #echo "power_saving" | tee /sys/class/drm/card?/gt/gt0/slpc_power_profile
     echo "power_saving" | tee /sys/class/drm/card?/device/tile*/gt*/freq*/power_profile
-    # disabling select cores (except core 0)
-    #disable_cores
-    # disable intel_pstate active mode (stop cpu from throttling up ever)
-    #echo "passive" >  /sys/devices/system/cpu/intel_pstate/status
-    # now we have to set the governor again because setting this to passive undid it
-    #echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
     # set screen to 60Hz
     kscreen_doctor 2880x1920@60
+    # PCIe stuff
+    echo "powersupersave" | tee /sys/module/pcie_aspm/parameters/policy
+    echo 1 | tee /sys/bus/pci/devices/*/power/control
+    set_rapl_limit package-0 10000000 13000000
     return 0
 }
 
 stop() {
-    [ "$USB_AUTOSUSPEND" = 1 ] && disable_usb_autosuspend
+    disable_usb_autosuspend
     disable_wifi_powersave
+    restore_hda_intel_powersave 10
+    disable_cpu_multicore_powersave
     #echo "base" | tee /sys/class/drm/card*/gt/gt0/slpc_power_profile
     echo "base" | tee /sys/class/drm/card?/device/tile*/gt*/freq*/power_profile
-    # reenable all cores
-    #echo 1 | tee /sys/devices/system/cpu/cpu*/online
-    # reenable active mode for intel_pstate
-    #echo "active" >  /sys/devices/system/cpu/intel_pstate/status
     # go back to 120Hz
     kscreen_doctor 2880x1920@120
+    echo "default" | tee /sys/module/pcie_aspm/parameters/policy
+    # no need to undo enabling runtime PM
+    #echo 1 | tee /sys/bus/pci/devices/*/power/control
+    set_rapl_limit package-0 35000000 60000000
     return 0
 }
 
